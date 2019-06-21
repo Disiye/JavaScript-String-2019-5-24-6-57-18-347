bd2f5a8c97910f3c18940cd36fee111e51d87d58
Disiye Weng <disiye@outlook.com>
2019-06-21 19:42:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,21 @@
 // 完成下面程序，将`name`变量中的字母全部转为为大写，输出：`'HELLO'`。
 var name = 'hello';
 // TODO write your code here...
-
+var upperName = name.toUpperCase();
+console.log(upperName);
 
 // 完成下面程序，将sentence变量中的所有单词首字母变为大写，输出：'Good Afternoon, Mr Mike.'。
 var sentence = 'good afternoon, mr mike.';
 //TODO write your code here...
-
+function initialCapital(str) {
+	return str.toLowerCase().replace(/\b([\w|']+)\b/g, function(word) {
+		return word.replace(word.charAt(0), word.charAt(0).toUpperCase());
+	});
+}
+console.log(initialCapital(sentence));
 
 // 完成下面程序，将money变量中的金额提取出来，输出：20。
 var money = '￥20';
 //TODO write your code here...
+var moneyNum = money.replace(/[^0-9]/ig,"");
+console.log(moneyNUm);
